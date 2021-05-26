fe50a37d0de47f33d2dd12e85cc2aa8170d8d87d
Shri <bhatshripad18@gmail.com>
2021-05-27 00:50:27 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/example/fitfreak/Splash.java
@@ -0,0 +1,27 @@
+package com.example.fitfreak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),Register.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
